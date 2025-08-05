ab80fbea10fc4809b42dc7e6878a1f8a53dd4561
yuntsai35 <amytsai563@gmail.com>
2025-08-05 23:18:04 +0100

    Add files via upload

---
+++ streamdashboard.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("Dataset.csv")
+
+years = st.multiselect("選擇年份", sorted(df["birthyear"].unique()), default=[2010])
+
+filtered_df = df[df["birthyear"].isin(years)]
+
+fig, ax = plt.subplots()
+sns.swarmplot(x="birthyear", y="dischage", data=filtered_df, size=0.5, ax=ax)
+
+ax.set_title("Birthyear vs Discharge Age")
+ax.set_xlabel("Birth Year")
+ax.set_ylabel("Discharge Age (Days)")
+
+st.pyplot(fig)
+
