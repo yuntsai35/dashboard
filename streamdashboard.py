61337f588a8ea403d8e40293bc8cf0d535b76880
yuntsai35 <amytsai563@gmail.com>
2025-08-05 23:26:44 +0100

    Update streamdashboard.py

--- streamdashboard.py
+++ streamdashboard.py
@@ -3,13 +3,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("https://drive.google.com/uc?id=1HzSqBa_3ZCOre-jB64-nwuPHiEwAvbRZ
+url = "https://drive.google.com/uc?id=1HzSqBa_3ZCOre-jB64-nwuPHiEwAvbRZ"
 
+@st.cache_data
+def load_data():
+    return pd.read_csv(url)
+
+df = load_data()
 years = st.multiselect("選擇年份", sorted(df["birthyear"].unique()), default=[2010])
 
 filtered_df = df[df["birthyear"].isin(years)]
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(10, 6))
 sns.swarmplot(x="birthyear", y="dischage", data=filtered_df, size=0.5, ax=ax)
 
 ax.set_title("Birthyear vs Discharge Age")
@@ -17,5 +22,6 @@ ax.set_xlabel("Birth Year")
 ax.set_ylabel("Discharge Age (Days)")
 
 st.pyplot(fig)
+
 
 
