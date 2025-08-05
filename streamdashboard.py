c740094c62377b7e8445f0dc0111ac0eb00824bc
yuntsai35 <amytsai563@gmail.com>
2025-08-05 23:42:36 +0100

    Update streamdashboard.py

--- streamdashboard.py
+++ streamdashboard.py
@@ -15,13 +15,14 @@ years = st.multiselect("Birth Year", sorted(df["birthyear"].unique()), default=[
 filtered_df = df[df["birthyear"].isin(years)]
 
 fig, ax = plt.subplots(figsize=(14, 8))
-sns.swarmplot(x="birthyear", y="dischage", data=filtered_df, size=0.5, ax=ax)
+sns.swarmplot(x="birthyear", y="dischage", data=filtered_df, size=2, ax=ax)
 
 ax.set_title("Birthyear vs Discharge Age")
 ax.set_xlabel("Birth Year")
 ax.set_ylabel("Discharge Age (Days)")
 
 st.pyplot(fig)
+
 
 
 
